6035da891262a8b29756c317c8b6b62bad60d71c
Onkar Ijare <ijareonkar2184@gmail.com>
2024-09-08 11:14:26 +0530

    username changed as email & emproved user page

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import User
-# Register your models here.
+from .forms import UserAdminCreationForm, UserAdminChangeForm
 
-admin.site.register(User)
+class UserAdmin(BaseUserAdmin):
+    search_fields = ['email']
+    form = UserAdminChangeForm  # For updating users
+    add_form = UserAdminCreationForm  # For creating new users
+
+    # Specify which fields to display in the admin list view
+    list_display = ['email', 'is_staff', 'is_active', 'is_superuser']
+
+    # Specify fields and fieldsets for add/change forms
+    fieldsets = (
+        ('Personal Info', {'fields': ('first_name', 'last_name','email', 'password')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('first_name', 'last_name', 'email', 'password', 'password_2', 'is_staff', 'is_active'),
+        }),
+    )
+
+    ordering = ['email']  # Specify default ordering by email
+
+
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,69 @@ from django.utils import timezone
 
 # Create your models here.
 
-class CustomUserManager(UserManager):
+# class CustomUserManager(UserManager):
+#     def _create_user(self, email, password, **extra_fields):
+#         if not email:
+#             raise ValueError("Email Required")
+        
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, **extra_fields)
+#         user.set_password(password)
+#         user.save(using=self._db)
+
+#         return user
+    
+#     def create_user(self, email=None, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', False)
+#         extra_fields.setdefault('is_superuser', False)
+#         return self._create_user(email, password, **extra_fields)
+    
+#     def create_superuser(self, email=None, password=None, **extra_fields):
+#         extra_fields.setdefault('is_staff', True)
+#         extra_fields.setdefault('is_superuser', True)
+#         return self._create_user(email, password, **extra_fields)
+    
+# class User(AbstractUser, PermissionsMixin):
+#     email = models.EmailField(blank=True,default='', unique=True)
+#     first_name = models.CharField(max_length=15, blank=True, default='')
+#     last_name = models.CharField(max_length=15, blank=True, default='')
+
+#     is_active = models.BooleanField(default=True)
+#     is_superuser = models.BooleanField(default=False)
+#     is_staff = models.BooleanField(default=False)
+
+#     date_joined = models.DateTimeField(default=timezone.now)
+#     last_login = models.DateTimeField(blank=True, null=True)
+
+#     objects = CustomUserManager()
+
+#     USERNAME_FIELD = 'email'
+#     EMAIL_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+
+#     class Meta:
+#         verbose_name = 'User'
+#         verbose_name_plural = 'Users'
+
+#     def get_short_name(self):
+#         return self.first_name or self.email.split('@')[0]
+
+# class CustomUser(AbstractUser):
+#     username = None
+#     first_name = models.TextField(max_length=15)
+#     last_name = models.TextField(max_length=15)
+#     email = models.EmailField(unique=True)
+
+#     objects = UserManager()
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
+
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("Email Required")
@@ -26,9 +88,9 @@ class CustomUserManager(UserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
         return self._create_user(email, password, **extra_fields)
-    
-class User(AbstractUser, PermissionsMixin):
-    email = models.EmailField(blank=True,default='', unique=True)
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=15, blank=True, default='')
     last_name = models.CharField(max_length=15, blank=True, default='')
 
@@ -42,7 +104,6 @@ class User(AbstractUser, PermissionsMixin):
     objects = CustomUserManager()
 
     USERNAME_FIELD = 'email'
-    EMAIL_FIELD = 'email'
     REQUIRED_FIELDS = []
 
     class Meta:
@@ -52,13 +113,3 @@ class User(AbstractUser, PermissionsMixin):
     def get_short_name(self):
         return self.first_name or self.email.split('@')[0]
 
-# class CustomUser(AbstractUser):
-#     username = None
-#     first_name = models.TextField(max_length=15)
-#     last_name = models.TextField(max_length=15)
-#     email = models.EmailField(unique=True)
-
-#     objects = UserManager()
-
-#     USERNAME_FIELD = 'email'
-#     REQUIRED_FIELDS = []
